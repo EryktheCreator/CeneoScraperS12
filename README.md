20b2cb816abb97be48b37bf11caafe78264c3d37
EryktheCreator <erni68@wp.pl>
2021-05-05 16:32:05 +0200

    05-05-2021 EK

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # CeneoScraperS12
 
 ## Etap 1: Ekstrakcja 
-### 1. analiza struktury opinii w serwisie [Ceneo.pl](https://www.ceneo.pl/)
+### 1. Analiza struktury opinii w serwisie [Ceneo.pl](https://www.ceneo.pl/)
 
 |Składowa|Selektor CSS|Nazwa zmiennej|Typ danych|
 |--------|------------|--------------|----------|
@@ -19,7 +19,7 @@
 |Dla ilu osób przydatna|span[id^="votes-yes"]|useful|int|
 |Dla ilu osób nieprzydatna|span[id^="votes-no"]|useless|int|
 
-### 2. pobranie składowych pojedynczej opinii
+### 2. Pobranie składowych pojedynczej opinii
 - pobranie kodu pojedynczej strony z opiniami
 - wyodrębnienie z kodu strony kodu pojedynczej opinii
 - pobranie do pojedynczych zmiennych poszczególnych składowych na podstawie selektorów
@@ -37,4 +37,19 @@
 - dodanie zapisu wszystkich opinii o produkcie do pliku .json
 
 ## Etap 4: Refactoring
-- 
+- zdefiniowanie funkcji do ekstrakcji pojedynczego elementu opinii
+- przygotowanie słownika opisującego składowe opinii wraz z ich selektorami
+- tworzenie słownika reprezentującego pojedynczą opinię przy wykorzystaniu wyrażenia słownikowego (dictionary comprehension)
+
+## Etap 5: Analiza statystyczna zbioru opinii o produkcie
+- wyświetlenie listy produktów, dla których pobrane zostały opinie
+- wczytanie opinii o wskazanym produkcie do obiektu DataFrame
+- obliczenie podstawowych statystyk
+    * średnia ocena produktu
+    * liczba opinii o produkcie
+    * liczba opinii dla których podana została liczba zalet
+    * liczba opinii dla których podana została liczba wad
+
+## Etap 6: Rysowanie wykresów opartych o dane z pobranych opinii
+- wykres słupkowy/kolumnowy obrazujący częstość występowania opinii z poszczególnymi ocenami
+- wykres kołowy obrazujący udział poszczególnych rodzajów rekomendacji w zbiorze opinii
--- analizer.py
+++ analizer.py
@@ -6,22 +6,40 @@ import matplotlib.pyplot as plt
 pd.set_option('display.max_columns', None)
 
 print(*listdir("opinions"), sep="\n")
-
 product_id = input("Podaj kod produktu: ")
 
 opinions = pd.read_json(f"./opinions/{product_id}.json")
 
-average_score = opinions.stars.mean().round(2)
+average_score = opinions.stars.mean()
+opinions_count = opinions.shape[0]
+pros_count = opinions.pros.astype(bool).sum()
+cons_count = opinions.cons.astype(bool).sum()
+print(f"""O produkcie dostępnych jest {opinions_count} opinii.
+Dla {pros_count} opinii podana została lista zalet, a dla {cons_count} lista wad.
+Średnia ocena produktu wyznaczona na podstawie liczby gwiazdek wynosi {average_score:.2f}.""")
 
 stars_count = opinions.stars.value_counts().reindex(np.arange(0,5.5,0.5), fill_value=0)
-
-stars_count.plot.bar()
+stars_count.plot.bar(color = "lightskyblue")
 plt.xlabel("Liczba gwiazdek")
 plt.ylabel("Liczba opinii")
-plt.show()
+plt.title("Częstośc występowania poszczególnych ocen produktu")
+plt.savefig(f"./figures/{product_id}_stars.png")
+plt.close()
 
-# pros_count i cons_count do ogarniecia
+recommendations = opinions.recomendation.value_counts(dropna = False).reindex([True, False, float("NaN")], fill_value=0)
+recommendations.plot.pie(
+    label = "",
+    labels = ["Polecam", "Nie polecam", "Nie mam zdania"],
+    autopct = "%1.1f%%",
+    pctdistance = 1.2,
+    labeldistance = 1.4,
+    colors = ["forestgreen", "crimson", "orange"]
+    )
+plt.title("Udział poszczególnych rekomendacji w ogólnej liczbie opinii")
+plt.legend(bbox_to_anchor = (1.0,1.0))
+plt.tight_layout()
+plt.savefig(f"./figures/{product_id}_rcmd.png", bbox_inches = "tight")
+plt.close()
 
-print(opinions)
-print(average_score)
-print(stars_count)
+stars_recommendations = pd.crosstab(opinions.stars, opinions.recomendation)
+print(stars_recommendations)
